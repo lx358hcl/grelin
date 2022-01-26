e12261ecbd45ea51b72361f68774f2adfeaaea7c
Fluttering Whale <edvardmchach@hotmail.com>
2022-01-26 14:30:12 +0100

    fiksa gtag

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js';
+import VueGtag from "vue-gtag-next";
 import firebaseApp from "@/firebase/firebaseconfig.js";
 
 firebaseApp.auth().onAuthStateChanged(() => {
-  createApp(App).use(router).mount('#app');
+  createApp(App).use(router, VueGtag, {
+    property: {
+      id: "UA-212580065-1"
+    }
+  }).mount('#app');
 });
 
 export default router;
